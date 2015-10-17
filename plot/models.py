08302bdc130da072275816be46def7a1d85cc0df
Jason Mickela <jason@rootid.in>
2015-10-17 14:20:52 -0700

    more model changes and added requirements file

--- plot/models.py
+++ plot/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class HomePlot(models.Model):
+    OPEN_ACCESS = 1
+    NEEDS_PERMISSION = 2
+
+    access_choices = (
+        (OPEN_ACCESS, _('Open Access')),
+        (NEEDS_PERMISSION, _('Needs Permission to access')),
+    )
+
+    LITTLE_SUN = 1
+    MEDIUM_SUN = 2
+    MANY_SUN = 3
+
+    sun_choices = (
+        (LITTLE_SUN, _('Mostly Shade')),
+        (MEDIUM_SUN, _('Partly Shady')),
+        (MANY_SUN, _('Lots of sun'))
+    )
+
+    size = models.FloatField(_('Plot Size'), help_text=_('Size of the plot in square feet.'))
+    access = models.IntegerField(_('Access Information'), choices=access_choices, help_text=_('Is this plot accessible all the time or only at certain times?'))
+    exchange_terms = models.TextField(_('Exchange Terms'), help_text=_('What do you want in exchange for this plot?'))
+
+    address = models.CharField(_('Street Address'), blank=False, max_length=255)
+    city = models.CharField(_('City'), default="Richmond", max_length=255)
+    zip = models.CharField(_('Zip Code'), )
+    state
+
+    sun_exposure
--- produce/models.py
+++ produce/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+class ProduceType(models.Model):
+    title = models.CharField(_('Type'), max_length=255)
+    description = models.TextField(_('Description'))
+
+class GardenItem(models.Model):
+    QUANTITY_LITTLE = 0
+    QUANTITY_LOT = 10
+
+    quantity_choices = (
+        (QUANTITY_LITTLE, _("A little")),
+        (QUANTITY_LOT, _("A lot")),
+    )
+
+
+
+    produce = models.ForeignKey(ProduceType)
+    quantity = models.IntegerField(_('Quantity'), help_text=_('How many do you have?'), choices=quantity_choices)
+    is_organic
+    size
+    description
