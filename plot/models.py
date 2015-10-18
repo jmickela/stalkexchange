7ca0b98abe49c9b62aaed11244a6b5f955691ad2
Jason Mickela <jason@rootid.in>
2015-10-18 11:11:21 -0700

    added facebook login to homepage

--- plot/models.py
+++ plot/models.py
@@ -16,14 +16,14 @@ class HomePlot(models.Model):
     MANY_SUN = 3
 
     sun_choices = (
-        (LITTLE_SUN, _('Mostly Shade')),
-        (MEDIUM_SUN, _('Partly Shady')),
-        (MANY_SUN, _('Lots of sun'))
+        (LITTLE_SUN, _('Shade')),
+        (MEDIUM_SUN, _('Partial Sun')),
+        (MANY_SUN, _('Full Sun'))
     )
 
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='home_plots')
     size = models.FloatField(_('Plot Size'), help_text=_('Size of the plot in square feet.'))
-    access = models.IntegerField(_('Access Information'), choices=access_choices, help_text=_('Is this plot accessible all the time or only at certain times?'))
+    access = models.IntegerField(_('Accessibility'), choices=access_choices, help_text=_('Is this plot accessible all the time or only at certain times?'))
     exchange_terms = models.TextField(_('Exchange Terms'), help_text=_('What do you want in exchange for this plot?'))
     sun_exposure = models.IntegerField(_('Sun Exposure'), choices=sun_choices)
 
